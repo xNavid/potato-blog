0473cb850532571882a55b3ae3980c60c896fe17
X_NAVID <navidcaesar@gmail.com>
2017-04-17 08:45:48 +0000

    Created new template, action and route for articles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
