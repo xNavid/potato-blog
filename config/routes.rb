4fac9bd43dab6f74197a68b95e9a626476fcf030
X_NAVID <navidcaesar@gmail.com>
2017-04-17 09:42:48 +0000

    Changed index route, added list of all posts on index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'articles#index'
   get 'about', to: 'pages#about'
   
   resources :articles
