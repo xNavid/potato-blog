5aa173b6027ccbd4e349c90b4557a7932bb3dc7a
X_NAVID <navidcaesar@gmail.com>
2017-04-17 09:59:05 +0000

    Added partials, reduced code redundancy, and added destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,12 @@ class ArticlesController < ApplicationController
 		end
 	end
 	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article was deleted!"
+		redirect_to articles_path
+	end
 	private
 	def article_params
 		params.require(:article).permit(:title, :description)
