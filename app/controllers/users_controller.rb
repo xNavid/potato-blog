1d173eed1024cfd6dcb5c4aacfcf4e66c77ecea7
X_NAVID <navidcaesar@gmail.com>
2017-04-19 15:56:58 +0000

    Now user gets Signed In up Signing Up automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome aboard #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
