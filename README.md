c125958106b1066af2547dd533e1dcb6d648ba3f
X_NAVID <navidcaesar@gmail.com>
2017-04-21 18:28:47 +0000

    Added styling to about page

---
+++ README.md
@@ -0,0 +1,14 @@
+# Potato
+### Express in one paragraph or go through other people's ideas
+
+## How it works
+
+1. Write your story :pencil:
+2. Add category :open_file_folder:
+3. Share! :smiley_cat:
+
+[Here's a Potato](http://potato-blog.herokuapp.com/)
+
+[MIT License](https://opensource.org/licenses/MIT)
+
+
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,10 +5,10 @@ class Article < ActiveRecord::Base
   has_many :categories, through: :article_categories
   
   validates :title, presence: true, 
-            length: { minimum: 3, maximum: 50 }
+            length: { minimum: 3, maximum: 99 }
             
   validates :description, presence: true, 
-            length: { minimum: 10, maximum: 300 }
+            length: { minimum: 10, maximum: 499 }
             
   validates :user_id, presence: true
   
