78e53d908796fd8d54fa467278385974a4195252
X_NAVID <navidcaesar@gmail.com>
2017-04-21 16:20:01 +0000

    Added edit category, restricted access only to admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,20 @@ class CategoriesController < ApplicationController
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Updated Category"
+      redirect_to category_path(@category)
+    else
+      
+    end
+  end
+  
   private
   def category_params
     params.require(:category).permit(:name)
